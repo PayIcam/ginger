4fcfe7cf59a7dcd599dd6eabac3cac0e3d1d9da0
Ithorion <antoine_giraud2@hotmail.fr>
2014-07-14 13:45:49 +0200

    ajout de la fonctionalité checkApp pour vérifier si l'application est reconue

--- index.php
+++ index.php
@@ -35,6 +35,8 @@ if($actions[1] == "find" || $actions[1] == "cotisations" || (!empty($actions[1])
 
 if($actions[1] == "badge" && !empty($actions[2]))
     $user = Users::getByBadge($actions[2]);
+else if($actions[1] == "checkApp" && !empty($actions[2]))
+    $user = Applications::checkApp($_GET["key"]);
 else if(!empty($actions[1]))
     $user = Users::getByLogin($actions[1]);
 
